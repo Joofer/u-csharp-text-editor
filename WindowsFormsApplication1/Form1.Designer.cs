8c1323df269d3d535327c65f35e57a7e3e75d7e9
Andrey Loskutov <joofer.sp@gmail.com>
2022-06-27 20:19:49 +0300

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
+            "Создать файл",
+            "Прочитать файл",
+            "Добавить в конец",
+            "Стереть данные из файла",
+            "Удалить файл",
+            "Зашифровать файл",
+            "Расшифровать файл"});
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "",
+            "Создать",
+            "Открыть",
+            "Сохранить",
+            "Сохранить как",
+            "Зашифровать файл",
+            "Расшифровать файл"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(198, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(13, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(998, 498);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 551);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Текстовый редактор";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 1:
+                    richTextBox1.Text = "";
+                    FileCreate();
+                    break;
+                case 2:
+                    FileRead();
+                    break;
+                case 3:
+                    FileWrite();
+                    break;
+                case 4:
+                    WriteAs();
+                    break;
+                case 5:
+                    FileEncrypt();
+                    break;
+                case 6:
+                    FileDecrypt();
+                    break;
+            }
+            Form1.ActiveForm.Text = openFileDialog1.FileName;
+        }
+
+        private void FileCreate()
+        {
+            StreamWriter stream;
+
+            saveFileDialog1.Filter = "txt files(*.txt)|*txt";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                stream = File.CreateText(saveFileDialog1.FileName + ".txt");
+                openFileDialog1.FileName = saveFileDialog1.FileName + ".txt";
+                stream.Close();
+            }
+        }
+
+        private void FileRead()
+        {
+            StreamReader stream;
+            string file = "";
+            string s;
+
+            openFileDialog1.Filter = "txt files(*.txt)|*txt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName != "")
+            {
+                file = openFileDialog1.FileName;
+            }
+
+            if (file != "")
+            {
+                stream = File.OpenText(file);
+                s = stream.ReadToEnd();
+                stream.Close();
+
+                richTextBox1.Text = s;
+            }
+        }
+
+        private void FileWrite()
+        {
+            StreamWriter stream;
+            string file = "";
+            string s;
+
+            s = richTextBox1.Text;
+
+            if (openFileDialog1.CheckFileExists == false)
+            {
+                openFileDialog1.Filter = "txt files(*.txt)|*txt";
+                openFileDialog1.FilterIndex = 1;
+                openFileDialog1.ShowDialog();
+                if (openFileDialog1.FileName != "")
+                {
+                    file = openFileDialog1.FileName;
+                }
+            }
+            else
+            {
+                file = openFileDialog1.FileName;
+            }
+
+            stream = File.CreateText(file);
+            stream.Write(s);
+            stream.Close();
+        }
+
+        private void WriteAs()
+        {
+            FileCreate();
+            FileWrite();
+        }
+
+        private void FileEncrypt()
+        {
+            
+        }
+
+        private void FileDecrypt()
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = "Слов: " + (richTextBox1.Text.Split(' ').Length + richTextBox1.Text.Split('\n').Length - 1) + " абзацев: " + richTextBox1.Text.Split('\n').Length + " символов: " + richTextBox1.Text.Length;
+        }
+    }
+}
